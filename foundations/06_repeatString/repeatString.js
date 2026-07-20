9b2651402e3945db0473182c8ff7d0c12eaf0af0
SamWlms10 <samwlms10@gmail.com>
2026-07-20 18:37:07 -0400

    Completed foundations assn 6 for Javascript TOP

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(string, count) {
+    if (count < 1) {
+        return "ERROR";
+    }
+    let newWord = "";
+    for (let i = count; i != 0; i--) {
+        newWord += string;
+    }
+    return newWord
 };
+console.log(repeatString("HI" , 3))
 
 // Do not edit below this line
 module.exports = repeatString;
