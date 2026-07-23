cd0475ea781c1c1e96b3ce4e4f35354cdd949d0a
SamWlms10 <samwlms10@gmail.com>
2026-07-23 19:24:03 -0400

    Finished assn 8 in TOP JS foundations

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...needsRemoved) {
+   return arr.filter(val => !needsRemoved.includes(val)) ;
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
