e8f11627b66704d2c6c448af14f630e02552b4e0
SamWlms10 <samwlms10@gmail.com>
2026-07-24 16:54:32 -0400

    Finished Assn 11 TOP JS foundations

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  const celsiusConversion = (fahrenheitTemp - 32) * (5/9)
+  return Number(celsiusConversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  const fahrenheitConversion = (celsiusTemp * (9/5)) + 32;
+  return Number(fahrenheitConversion.toFixed(1));
 };
 
 // Do not edit below this line
