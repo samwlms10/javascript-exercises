b9f3f66ac177981f4dfd5daf1986e558c1c08e5c
SamWlms10 <samwlms10@gmail.com>
2026-07-24 15:57:32 -0400

    Finished assn 9 in TOP JS foundations

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    let addAtIndex = 1
+    let lastNum = nums.at(-1);
 
+    for (const num of nums){
+        if (num < 0 || num % 1 != 0 || typeof num != "number"){
+            return "ERROR";
+        }
+    }
+    if (nums[0] < lastNum){
+        for (let i = nums[0] + 1; i < lastNum ; i++){
+            nums.splice(addAtIndex, 0, i);
+            addAtIndex++
+        }
+    }
+    else{
+        for (let i = nums[0] - 1; i > lastNum ; i--){
+            nums.splice(addAtIndex, 0, i);
+            addAtIndex++
+        }
+    }
+const sum = function(accumulator, element){
+    return accumulator + element;
+    }
+    return nums.reduce(sum);
 };
-
 // Do not edit below this line
 module.exports = sumAll;
